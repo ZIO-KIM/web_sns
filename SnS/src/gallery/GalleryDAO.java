22cd48e253a34c3c36e082547a97732d8b5654e0
sseun <sseun@DESKTOP-4O263MA>
2019-09-18 07:45:12 +0900

    갤러리 버그 수정 및 수정

--- SnS/src/gallery/GalleryDAO.java
+++ SnS/src/gallery/GalleryDAO.java
@@ -86,9 +86,9 @@ public class GalleryDAO {
 			pstmt = conn.prepareStatement(SQL);
 			pstmt.setString(1, galTitle);
 			pstmt.setString(2, galContent);
-			pstmt.setInt(3, galID);
-			pstmt.setString(4, galFile);
-			pstmt.setString(5, galRealFile);
+			pstmt.setString(3, galFile);
+			pstmt.setString(4, galRealFile);
+			pstmt.setInt(5, galID);
 			return pstmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
